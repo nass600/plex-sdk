457fe752a13827dd4f9f4ce179efd88fc15564e0
Ignacio Velazquez <ivelazquez85@gmail.com>
2023-01-12 02:35:22 +0100

    fix: amending semantic release config

--- release.config.ts
+++ release.config.ts
@@ -6,16 +6,16 @@ export default {
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
         ['@semantic-release/npm', { npmPublish: true }],
-        [
-            '@semantic-release/git',
-            {
-                assets: [
-                    'package.json',
-                    'package-lock.json'
-                ],
-                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-            }
-        ],
-        '@semantic-release/github'
+        ['@semantic-release/github', {
+            assets: ['dist/**']
+        }],
+        ['@semantic-release/git', {
+            assets: [
+                'dist/**',
+                'package.json',
+                'package-lock.json'
+            ],
+            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }]
     ]
 }
